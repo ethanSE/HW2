bdcb910e215c9b7ecca63b041e76f8a718cc1573
Ethan Samuels-Ellingson <ethansamuelsellingson@gmail.com>
2021-01-04 23:48:12 -0800

    add code to generate empty folders in ~/.data

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //dependenies
 const server = require('./lib/server');
-const cli = require('./lib/cli')
+const cli = require('./lib/cli');
+const helpers = require('./lib/helpers');
 
 let app = {};
 
 app.init = () => {
+    //generate require files
+    helpers.generateFiles();
+
     //start the server
     server.init();
 
